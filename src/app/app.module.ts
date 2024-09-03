e16ce0b20a8dd05d9b7684aaf1fc288cf4668816
randika-chathuranga <randikachathuranga219@gmail.com>
2024-09-03 12:02:37 +0530

    initial components were added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { DealDataComponent } from './components/deal-data/deal-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TopBarComponent,
+    SideBarComponent,
+    DealDataComponent
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/deal-data/deal-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deal-data',
+  templateUrl: './deal-data.component.html',
+  styleUrls: ['./deal-data.component.css']
+})
+export class DealDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
